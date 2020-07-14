08541dcfeeab12913532215d18fd247bba490bc9
Mitchell Rodríguez <mitrodle@gmail.com>
2020-07-14 09:17:12 -0500

    Modificaciones en DAO de Estado

--- PortalWeb/src/net/portal/dao/MySqlEstadoDAO.java
+++ PortalWeb/src/net/portal/dao/MySqlEstadoDAO.java
@@ -2,6 +2,8 @@ package net.portal.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import net.portal.entidad.Estado;
@@ -41,19 +43,92 @@ public class MySqlEstadoDAO implements EstadoDAO{
 	@Override
 	public int updateEstado(Estado bean) {
 		int estado = -1;
+		
+		Connection cn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			cn = MySqlBDConexion.getConexion();
+			String sql = "Update estado set estado_nombre = ? where estado_id = ?";
+			pstm = cn.prepareStatement(sql);
+			pstm.setString(1, bean.getEstado_nombre());
+			pstm.setInt(2, bean.getEstado_id());
+			estado = pstm.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstm != null) pstm.close();
+				if(cn != null) cn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
 		return estado;
 	}
 
 	@Override
 	public int deleteEstado(int codigo) {
-		// TODO Auto-generated method stub
-		return 0;
+		int estado = -1;
+		
+		Connection cn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			cn = MySqlBDConexion.getConexion();
+			String sql = "Delete from estado where estado_id = ?";
+			pstm = cn.prepareStatement(sql);
+			pstm.setInt(1, codigo);
+			estado = pstm.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstm != null) pstm.close();
+				if(cn != null) cn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+		return estado;
 	}
 
 	@Override
 	public List<Estado> listEstados() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Estado> lista = new ArrayList<Estado>();
+		
+		Estado bean = null;
+		Connection cn = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		
+		try {
+			cn = MySqlBDConexion.getConexion();
+			String sql = "Select estado_id, estado_nombre from estado";
+			pstm = cn.prepareStatement(sql);
+			rs = pstm.executeQuery();
+			
+			while(rs.next()) {
+				bean = new Estado();
+				bean.setEstado_id(rs.getInt(1));
+				bean.setEstado_nombre(rs.getString(2));
+				lista.add(bean);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstm != null) pstm.close();
+				if(cn != null) cn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+		return lista;
 	}
 
 }
